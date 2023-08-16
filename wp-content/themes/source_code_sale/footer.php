49663c8924fbf856668c8f114c33e07aa5360b74
longtwokey #php_dev <longshare9201@gmail.com>
2023-08-16 11:51:29 +0700

    update theme

--- wp-content/themes/source_code_sale/footer.php
+++ wp-content/themes/source_code_sale/footer.php
@@ -1,3 +1,21 @@
+<script src="<?php echo getPublicFile('theme/vendor/jquery/jquery-3.2.1.min.js'); ?>"></script>
+<script src="<?php echo getPublicFile('theme/vendor/animsition/js/animsition.min.js'); ?>"></script>
+<script src="<?php echo getPublicFile('theme/vendor/bootstrap/js/popper.js'); ?>"></script>
+<script src="<?php echo getPublicFile('theme/vendor/bootstrap/js/bootstrap.min.js'); ?>"></script>
+<script src="<?php echo getPublicFile('theme/vendor/select2/select2.min.js'); ?>"></script>
+<script src="<?php echo getPublicFile('theme/vendor/daterangepicker/moment.min.js'); ?>"></script>
+<script src="<?php echo getPublicFile('theme/vendor/daterangepicker/daterangepicker.js'); ?>"></script>
+<script src="<?php echo getPublicFile('theme/vendor/slick/slick.min.js'); ?>"></script>
+<script src="<?php echo getPublicFile('theme/js/slick-custom.js'); ?>"></script>
+<script src="<?php echo getPublicFile('theme/vendor/parallax100/parallax100.js'); ?>"></script>
+<script src="<?php echo getPublicFile('theme/vendor/MagnificPopup/jquery.magnific-popup.min.js'); ?>"></script>
+<script src="<?php echo getPublicFile('theme/vendor/isotope/isotope.pkgd.min.js'); ?>"></script>
+<script src="<?php echo getPublicFile('theme/vendor/sweetalert/sweetalert.min.js'); ?>"></script>
+<script src="<?php echo getPublicFile('theme/vendor/perfect-scrollbar/perfect-scrollbar.min.js'); ?>"></script>
+<script src="<?php echo getPublicFile('theme/js/main.js'); ?>"></script>
+
+
+
 <div class="container">
     <footer class="py-5">
         <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
--- wp-content/themes/source_code_sale/head.php
+++ wp-content/themes/source_code_sale/head.php
@@ -1,39 +1,29 @@
-<script src="<?= getPublicFile('lib/jquery.min.js'); ?>"></script>
-<script src="<?= getPublicFile('lib/popper.min.js'); ?>"></script>
-<script src="<?= getPublicFile('lib/bootstrap5/js/bootstrap.bundle.min.js'); ?>"></script>
-<link href="<?= getPublicFile('lib/bootstrap5/css/bootstrap.min.css'); ?>" rel="stylesheet">
-<script src="<?= getPublicFile('lib/splide/dist/js/splide.js'); ?>"></script>
-<link rel="stylesheet" href="<?= getPublicFile('lib/splide/dist/css/splide.min.css'); ?>">
-<link rel="stylesheet" href="<?= getPublicFile('lib/fontawesome/css/all.css'); ?>">
-<link rel="stylesheet" href="<?= getPublicFile('css/base.css'); ?>">
-
-<?php
-    $urlNodejsSocketIo = get_field('url_nodejs_socket_io', 'options');
-?>
-
-<?php if (!empty($urlNodejsSocketIo)): ?>
-    <script src="<?= $urlNodejsSocketIo; ?>"></script>
-    <script>
-        var socket = io('<?= str_replace('socket.io/socket.io.js', '', $urlNodejsSocketIo) ?>');
-
-        $(document).ready(function () {
-            socket.on('server-send-list-user-online', (data) => {
-                $('#user-online-count').text(data.count);
-            });
-        });
-    </script>
-<?php endif; ?>
-
-
-
-<meta charset="UTF-8">
-<meta name="viewport"
-      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<?php if (is_home()): ?>
-    <title><?php bloginfo('name') ?></title>
-<?php else: ?>
-    <title><?php wp_title('', true, 'right'); ?> - <?php bloginfo('name') ?></title>
-<?php endif; ?>
-
-<meta name="zalo-platform-site-verification" content="EjtYAiZDCY0rvweRmz4u62YlepV4t7LhD3G" />
+<!--===============================================================================================-->
+<link rel="icon" type="image/png" href="<?php echo getPublicFile('theme/images/icons/favicon.png'); ?>"/>
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="<?php echo getPublicFile('theme/vendor/bootstrap/css/bootstrap.min.css'); ?>">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="<?php echo getPublicFile('theme/fonts/font-awesome-4.7.0/css/font-awesome.min.css'); ?>">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="<?php echo getPublicFile('theme/fonts/iconic/css/material-design-iconic-font.min.css'); ?>">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="<?php echo getPublicFile('theme/fonts/linearicons-v1.0.0/icon-font.min.css'); ?>">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="<?php echo getPublicFile('theme/vendor/animate/animate.css'); ?>">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="<?php echo getPublicFile('theme/vendor/css-hamburgers/hamburgers.min.css'); ?>">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="<?php echo getPublicFile('theme/vendor/animsition/css/animsition.min.css'); ?>">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="<?php echo getPublicFile('theme/vendor/select2/select2.min.css'); ?>">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="<?php echo getPublicFile('theme/vendor/daterangepicker/daterangepicker.css'); ?>">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="<?php echo getPublicFile('theme/vendor/slick/slick.css'); ?>">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="<?php echo getPublicFile('theme/vendor/MagnificPopup/magnific-popup.css'); ?>">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="<?php echo getPublicFile('theme/vendor/perfect-scrollbar/perfect-scrollbar.css'); ?>">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="<?php echo getPublicFile('theme/css/util.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo getPublicFile('theme/css/main.css'); ?>">
